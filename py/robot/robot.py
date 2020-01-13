71ff88df1fff5236decceb0bcaa4275ff3a9fdae
AndChen153 <achen1192@gmail.com>
2020-01-13 13:34:38 -0600

    new motor assignments

---
+++ py/robot/robot.py
@@ -0,0 +1,54 @@
+import wpilib
+import ctre
+import magicbot
+import navx
+from components import drivetrain
+#targeting
+from controllers import alignment_controller
+
+CONTROLLER_LEFT = wpilib.XboxController.Hand.kLeft
+CONTROLLER_RIGHT = wpilib.XboxController.Hand.kRight
+
+
+class SpartaBot(magicbot.MagicRobot):
+
+    drivetrain = drivetrain.Drivetrain
+    #targeting = targeting.Targeting
+
+
+    def createObjects(self):
+        self.drive_controller = wpilib.XboxController(0)
+        self.compressor = wpilib.Compressor()
+
+        self.drivetrain_left_motor_master = ctre.WPI_TalonSRX(6)
+        self.drivetrain_left_motor_slave = ctre.WPI_TalonSRX(5)
+        self.drivetrain_right_motor_master = ctre.WPI_TalonSRX(8)
+        self.drivetrain_right_motor_slave = ctre.WPI_TalonSRX(7)
+        self.drivetrain_shifter_solenoid = wpilib.Solenoid(2)
+        self.navx = navx.AHRS.create_spi()
+
+
+    def autonomousInit(self):
+        pass
+
+    def autonomousPeriodic(self):
+        pass
+
+    def teleopInit(self):
+        #self.drivetrain.reset_angle_correction()
+        pass
+
+
+    def teleopPeriodic(self):
+        angle = self.drive_controller.getX(CONTROLLER_RIGHT)
+        self.drivetrain.angle_corrected_differential_drive(
+            self.drive_controller.getY(CONTROLLER_LEFT), angle)
+
+        
+        if self.drive_controller.getStickButtonReleased(CONTROLLER_LEFT):
+            self.drivetrain.shift_toggle()
+
+        
+
+if __name__ == '__main__':
+    wpilib.run(SpartaBot)
