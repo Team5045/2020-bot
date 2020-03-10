31d8b3ccd0bd7658b8e2ee61b8718962d41b230f
Andrew Chen <achen1192@gmail.com>
2020-03-09 19:43:21 -0500

    Jie's commit

--- py/robot/components/shooter.py
+++ py/robot/components/shooter.py
@@ -1,45 +1,44 @@
 import wpilib
-from ctre import WPI_VictorSPX
-from enum import IntEnum
+from ctre import WPI_TalonFX
 from wpilib import DoubleSolenoid
 
-class HoodState(IntEnum):
-    EXTENDED = 0
-    RETRACTED = 1
 
 class Shooter:
 
-    motor_master = WPI_VictorSPX
-    motor_slave = WPI_VictorSPX
+    motor_master = WPI_TalonFX
+    motor_slave = WPI_TalonFX
     hood_solenoid = DoubleSolenoid
+
+    state = False
 
     def setup(self):
         #shooter
         self.speed=0.0
-        self.stopstart=False
+
         #set slave motor
-        self.motor_slave.set(WPI_VictorSPX.ControlMode.Follower,
+        self.motor_slave.set(WPI_TalonFX.ControlMode.Follower,
                                   self.motor_master.getDeviceID())
         self.motor_master.setInverted(True)
 
         #hood
-        self.state = HoodState.RETRACTED
+        self.state = False
 
     def run_shooter(self, speed):
         self.motor_master.set(speed)
         self.speed = speed
     
     def switch(self):
-        if self.state == HoodState.EXTENDED:
-            self.state = HoodState.RETRACTED
-        elif self.state == HoodState.RETRACTED:
-            self.state = HoodState.EXTENDED
+        print("switch")
+        if self.state == False:
+            self.state = True
+        elif self.state == True:
+            self.state = False
 
     def extend(self):
-        self.state = HoodState.EXTENDED
+        self.state = True
 
     def retract(self):
-        self.state = HoodState.RETRACTED
+        self.state = False
 
     def get_state(self):
         return {
@@ -47,9 +46,13 @@ class Shooter:
         }
 
     def execute(self):
-        if self.state == HoodState.RETRACTED:
+        print("execute")
+        if self.state == True:
             self.hood_solenoid.set(DoubleSolenoid.Value.kForward)
-        elif self.state == HoodState.EXTENDED:
+        elif self.state == False:
             self.hood_solenoid.set(DoubleSolenoid.Value.kReverse)
 
-        self.run_shooter(self.speed)
+        if self.enable:
+            self.run_shooter(self.speed)
+        else:
+            self.run_shooter(0)
--- py/robot/robot.py
+++ py/robot/robot.py
@@ -3,43 +3,30 @@ import ctre
 import magicbot
 import navx
 import wpilib.drive
-from enum import IntEnum
-from wpilib import Solenoid, DoubleSolenoid
-from components import shooterFalcon #, shifter, intake, tower
-#from components.shooterFalcon import Shooter
 from wpilib.drive import DifferentialDrive
-from networktables import NetworkTables
+from wpilib import Solenoid, DoubleSolenoid
+from components import shooter, drivetrainFalcon
 
-'''
-NetworkTables.initialize(server='roborio-5045-frc.local')
-
-sd = NetworkTables.getTable('SmartDashboard')
-sd.putNumber('someNumber', 1234)
-otherNumber = sd.getNumber('otherNumber')
-'''
 
 CONTROLLER_LEFT = wpilib.XboxController.Hand.kLeftHand
 CONTROLLER_RIGHT = wpilib.XboxController.Hand.kRightHand
 
 
 class SpartaBot(magicbot.MagicRobot):
-    '''
-    shooter = shooterFalcon.Shooter()
-    shifter: Shifter
-    intake: Intake
-    tower: Tower
-    '''
+    shooter = shooter.Shooter()
+    drivetrain = drivetrainFalcon.Drivetrain
 
-    def createObjects(self):        
+    def createObjects(self):
         self.drive_controller = wpilib.XboxController(0)
 
-        #drivetrain
-        self.drivetrain_left_motor_master = ctre.WPI_TalonFX(1)
-        self.drivetrain_left_motor_slave = ctre.WPI_TalonFX(2)
-        self.drivetrain_left_motor_slave2 = ctre.WPI_TalonFX(3)
-        self.drivetrain_right_motor_master = ctre.WPI_TalonFX(4)
-        self.drivetrain_right_motor_slave = ctre.WPI_TalonFX(5)
-        self.drivetrain_right_motor_slave2 = ctre.WPI_TalonFX(6)
+        self.drivetrain_left_motor_master = ctre.WPI_TalonSRX(1)
+        self.drivetrain_left_motor_slave = ctre.WPI_TalonSRX(2)
+        self.drivetrain_left_motor_slave2 = ctre.WPI_TalonSRX(3)
+
+        self.drivetrain_right_motor_master = ctre.WPI_TalonSRX(4)
+        self.drivetrain_right_motor_slave = ctre.WPI_TalonSRX(5)
+        self.drivetrain_right_motor_slave2 = ctre.WPI_TalonSRX(6)
+
         self.left = wpilib.SpeedControllerGroup(
             self.drivetrain_left_motor_master, self.drivetrain_left_motor_slave, self.drivetrain_left_motor_slave2
             )
@@ -51,23 +38,14 @@ class SpartaBot(magicbot.MagicRobot):
 
         self.shifter_shiftsolenoid = wpilib.Solenoid(1)
 
-
-
-        #shooter
-        self.hood_solenoid = wpilib.DoubleSolenoid(4,5)
-        self.shooter_motor = ctre.WPI_TalonFX(21)
-        self.shooter_motor_slave = ctre.WPI_TalonFX(20)
-
         #intake
         self.intake_roller_motor = ctre.WPI_TalonSRX(7)
         self.intake_arm_solenoid = wpilib.DoubleSolenoid(2,3)
 
-        #feed
-        self.tower_feed_motor_master = ctre.WPI_TalonSRX(8)
-        self.tower_feed_motor_slave = ctre.WPI_TalonSRX(10)
-
-        #tower
-        self.tower_motor = ctre.WPI_TalonSRX(12)
+        #shooter
+        self.motor_master = ctre.WPI_TalonFX(21)
+        self.motor_slave = ctre.WPI_TalonFX(20)
+        self.hood_solenoid = wpilib.DoubleSolenoid(4,5)
 
     def autonomousInit(self):
         pass
@@ -81,83 +59,44 @@ class SpartaBot(magicbot.MagicRobot):
         self.intake_arm_solenoid.set(DoubleSolenoid.Value.kReverse)
 
     def teleopPeriodic(self):
-
-        angle = self.drive_controller.getX(CONTROLLER_RIGHT)
         speed = self.drive_controller.getY(CONTROLLER_LEFT)
+        angle = self.drive_controller.getX(CONTROLLER_RIGHT)
         if (abs(angle) > 0.05 or abs(speed) > 0.05):
             self.drive.arcadeDrive(speed, -angle, True) 
         else:
-            self.drive.curvatureDrive(0, 0, True)
-
-        #shifter
+            self.drive.arcadeDrive(0, 0, True)
+        
         if self.drive_controller.getStickButtonReleased(CONTROLLER_LEFT):
             self.shifter_shiftsolenoid.set(False)
         if self.drive_controller.getStickButtonReleased(CONTROLLER_RIGHT):
             self.shifter_shiftsolenoid.set(True)
-        
+
         #shooter
         if self.drive_controller.getTriggerAxis(CONTROLLER_RIGHT)>0.75:
-            #self.shooter.run_shooter(0.95)
-            self.shooter_motor.set(-self.drive_controller.getTriggerAxis(CONTROLLER_RIGHT))
-            self.shooter_motor_slave.set(self.drive_controller.getTriggerAxis(CONTROLLER_RIGHT))
-        elif self.drive_controller.getTriggerAxis(CONTROLLER_RIGHT)>0.3:
-            #self.shooter.run_shooter(0.5)
-            self.shooter_motor.set(-0.5)
-            self.shooter_motor_slave.set(0.5)
+            self.shooter.run_shooter(self, -0.6)
         else:
-            self.shooter_motor.stopMotor()
-            self.shooter_motor_slave.stopMotor()
+            self.motor_master.stopMotor()
+            self.motor_slave.stopMotor()
 
-        #hood
+        #shooter hood
+
         if self.drive_controller.getStartButtonReleased():
-            self.hood_solenoid.set(DoubleSolenoid.Value.kForward)
-        elif self.drive_controller.getBackButtonReleased():
             self.hood_solenoid.set(DoubleSolenoid.Value.kReverse)
+        elif self.drive_controller.getBackButtonReleased():
+            self.hood_solenoid.set(DoubleSolenoid.Value.kForward)
+        
+        # if self.drive_controller.getBButtonReleased():
+        #     self.shooter.switch()
+        
 
-        #intake/feed
-        if self.drive_controller.getBumper(CONTROLLER_RIGHT):
-            self.intake_roller_motor.set(-0.9)
-            self.tower_feed_motor_master.set(0.3)
-            self.tower_feed_motor_slave.set(0.3)
-        elif self.drive_controller.getAButton():
-            self.intake_roller_motor.set(0.9)
-            self.tower_feed_motor_master.set(-0.4)
-            self.tower_feed_motor_slave.set(-0.4)
-        else:
-            self.intake_roller_motor.stopMotor()       
-            self.tower_feed_motor_master.stopMotor()
-            self.tower_feed_motor_slave.stopMotor()
-
-        #intake arm deploy
+        #deploy intake
         if self.drive_controller.getYButtonReleased():
             self.intake_arm_solenoid.set(DoubleSolenoid.Value.kForward)
         elif self.drive_controller.getXButtonReleased():
             self.intake_arm_solenoid.set(DoubleSolenoid.Value.kReverse)
 
 
-        #tower
-        '''
-        if self.drive_controller.getTriggerAxis(CONTROLLER_LEFT)>0.2:
-            self.tower_motor.set(-self.drive_controller.getTriggerAxis(CONTROLLER_LEFT))
-        else:
-            self.tower_motor.stopMotor()
-
-
-        if self.drive_controller.getBumper(CONTROLLER_LEFT):
-            self.tower.move_incremental(35000)
-        elif self.drive_controller.getBButtonReleased():
-            self.tower.move_incremental(-5000)
-        '''
-        if self.drive_controller.getTriggerAxis(CONTROLLER_LEFT)>0.2:
-            self.tower_motor.set(-self.drive_controller.getTriggerAxis(CONTROLLER_LEFT))
-        elif self.drive_controller.getBumper(CONTROLLER_LEFT):
-            self.tower_motor.set(0.5)
-        else:
-            self.tower_motor.set(0)
-
-             
-        
-        
 
 if __name__ == '__main__':
     wpilib.run(SpartaBot)
+
